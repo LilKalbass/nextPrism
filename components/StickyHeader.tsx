bea739a586693708764851122591eba09d9818f0
Artem Dimoglov <adimoglov@gmail.com>
2025-06-24 14:24:00 +0300

    q

--- components/StickyHeader.tsx
+++ components/StickyHeader.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from "react";
 
 export default function StickyHeader() {
     const topHeaderRef = useRef<HTMLDivElement>(null);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [isStuck, setIsStuck] = useState(false);
 
     useEffect(() => {
